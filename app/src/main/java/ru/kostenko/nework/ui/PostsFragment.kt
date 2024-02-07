b91ed0c78ef082276d06c2769a2ea49b30b86286
Kostenko Konstantin <thetanqel@gmail.com>
2024-02-07 15:53:03 +0400

    Переделал лэйаут фрагмента постов, и сдела топ бар для него

--- app/src/main/java/ru/kostenko/nework/ui/PostsFragment.kt
+++ app/src/main/java/ru/kostenko/nework/ui/PostsFragment.kt
@@ -1,16 +1,22 @@
 package ru.kostenko.nework.ui
 
+
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.Fragment
 import dagger.hilt.android.AndroidEntryPoint
+import ru.kostenko.nework.R
 import ru.kostenko.nework.databinding.FragmentPostsBinding
+
 
 @AndroidEntryPoint
 class PostsFragment: Fragment() {
+
+    private lateinit var toolbar: Toolbar
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -18,11 +24,29 @@ class PostsFragment: Fragment() {
         savedInstanceState: Bundle?
     ): View {
         val binding = FragmentPostsBinding.inflate(layoutInflater)
+        setHasOptionsMenu(true)
+        toolbar = binding.root.findViewById<Toolbar>(R.id.toolbar)
+
+
+        toolbar.apply{
+            setTitle(R.string.app_name)
+            inflateMenu(R.menu.auth_menu)
+            setOnMenuItemClickListener {
+                when (it.itemId) {
+                    R.id.authentication -> {
+                        Toast.makeText(this.context, "авторизуйся", Toast.LENGTH_SHORT).show()
+                        true
+                    }
+                    else -> false
+                }
+            }
+        }
 
         binding.addPost.setOnClickListener {
             Toast.makeText(this.context, "добавь пост", Toast.LENGTH_SHORT).show()
         }
     return binding.root
     }
+
 
 }
